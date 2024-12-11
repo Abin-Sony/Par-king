51f61ad9d06b887d413821c983a8b3dae47832e6
lekshmibalachandrann <lekshmibalachandran501@gmail.com>
2024-12-11 19:47:23 +0530

    modification on parking places

--- src/pages/ParkingPlaces.jsx
+++ src/pages/ParkingPlaces.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 const ParkingPlace = () => {
     const locations = [
@@ -31,7 +33,7 @@ const ParkingPlace = () => {
 
 
 
-            {/* Location Cards */}
+            
             <div className="row mt-5">
                 {locations.map((location) => (
                     <div className="col-md-4 mb-4" key={location.id}>
@@ -44,14 +46,9 @@ const ParkingPlace = () => {
                             <div className="card-body">
                                 <h5 className="card-title">{location.name}</h5>
 
-                                <a
-                                    href=""
-                                    target="_blank"
-
-                                    className="btn btn-danger shadow"
-                                >
-                                    Book Slot
-                                </a>
+                               <Link to={'/BookingForm'}><button className="btn btn-danger">Book Slot</button></Link> 
+                                    
+                               
                             </div>
                         </div>
                     </div>
